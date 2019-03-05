3ac08ab6df88cc032da739858f441cf597fa89f2
PiotrGabrys <piotrek.gabrys@gmail.com>
2019-03-05 23:12:29 +0100

    setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='prunedcv',
+    author='Piotr Gabryś',
+    author_email='piotrek.gabrys@gmail.com',
+    version='0.1',
+    packages=['prunedcv',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    url="https://github.com/PiotrekGa/pruned-cv"
+)
