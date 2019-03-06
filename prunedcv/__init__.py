ec2c66e0ee894dbfbae4d83b2128020fd73de74d
Piotr Gabrys <piotrgabrys@Piotrs-MacBook-Pro.local>
2019-03-06 10:14:53 +0100

    names changed to defauls sklearn

--- prunedcv/__init__.py
+++ prunedcv/__init__.py
@@ -4,23 +4,23 @@ from sklearn import metrics
 
 class PrunerCV:
 
-    def __init__(self, n_folds, tolerance):
+    def __init__(self, n_splits, tolerance):
 
-        if not isinstance(n_folds, int):
+        if not isinstance(n_splits, int):
             raise TypeError
-        if n_folds < 2:
+        if n_splits < 2:
             raise ValueError
         if not isinstance(tolerance, float):
             raise TypeError
         if tolerance < 0:
             raise ValueError
 
-        self.n_folds = n_folds
+        self.n_splits = n_splits
         self.tolerance_scaler = tolerance + 1
         self.prun = False
         self.cross_val_score = 0.0
-        self.current_folds_list = []
-        self.best_folds_list = []
+        self.current_splits_list = []
+        self.best_splits_list = []
         self.first_run = True
         self.best_model = None
         self.model = None
@@ -28,7 +28,7 @@ class PrunerCV:
     def cross_validate_score(self, model, x, y, metric='mse', shuffle=False, random_state=42):
 
         self.model = model
-        kf = KFold(n_splits=self.n_folds, shuffle=shuffle, random_state=random_state)
+        kf = KFold(n_splits=self.n_splits, shuffle=shuffle, random_state=random_state)
         for train_idx, test_idx in kf.split(x, y):
             if not self.prun:
                 x_train, y_train = x[train_idx], y[train_idx]
@@ -37,56 +37,56 @@ class PrunerCV:
                 y_test_teor = self.model.predict(x_test)
 
                 if metric == 'mse':
-                    self.add_fold_value_and_prun(metrics.mean_squared_error(y_test, y_test_teor))
+                    self.add_split_value_and_prun(metrics.mean_squared_error(y_test, y_test_teor))
                 if metric == 'mae':
-                    self.add_fold_value_and_prun(metrics.mean_absolute_error(y_test, y_test_teor))
+                    self.add_split_value_and_prun(metrics.mean_absolute_error(y_test, y_test_teor))
 
         self.prun = False
         return self.cross_val_score
 
-    def add_fold_value_and_prun(self, value):
+    def add_split_value_and_prun(self, value):
 
         if not isinstance(value, float):
             raise TypeError
 
-        if len(self.current_folds_list) == 0:
+        if len(self.current_splits_list) == 0:
             self.prun = False
 
-        self.current_folds_list.append(value)
+        self.current_splits_list.append(value)
 
         if self.first_run:
-            self._populate_best_folds_list_at_first_run(value)
+            self._populate_best_splits_list_at_first_run(value)
 
         self._decide_prun()
 
-        if len(self.current_folds_list) == self.n_folds:
-            self._serve_last_fold()
+        if len(self.current_splits_list) == self.n_splits:
+            self._serve_last_split()
 
-    def _populate_best_folds_list_at_first_run(self, value):
+    def _populate_best_splits_list_at_first_run(self, value):
 
-        self.best_folds_list.append(value)
+        self.best_splits_list.append(value)
 
-        if len(self.best_folds_list) == self.n_folds:
+        if len(self.best_splits_list) == self.n_splits:
             self.first_run = False
 
     def _decide_prun(self):
 
-        fold_num = len(self.current_folds_list)
-        mean_best_folds = sum(self.best_folds_list[:fold_num]) / fold_num
-        mean_curr_folds = sum(self.current_folds_list) / fold_num
+        split_num = len(self.current_splits_list)
+        mean_best_splits = sum(self.best_splits_list[:split_num]) / split_num
+        mean_curr_splits = sum(self.current_splits_list) / split_num
 
-        if self.n_folds > fold_num >= 2:
-            if mean_best_folds * self.tolerance_scaler < mean_curr_folds:
+        if self.n_splits > split_num >= 2:
+            if mean_best_splits * self.tolerance_scaler < mean_curr_splits:
                 self.prun = True
-                self.cross_val_score = mean_curr_folds
-                self.current_folds_list = []
-                print('trial pruned at {} fold'.format(fold_num))
+                self.cross_val_score = mean_curr_splits
+                self.current_splits_list = []
+                print('trial pruned at {} fold'.format(split_num))
 
-    def _serve_last_fold(self):
+    def _serve_last_split(self):
 
-        if sum(self.best_folds_list) > sum(self.current_folds_list):
-            self.best_folds_list = self.current_folds_list
+        if sum(self.best_splits_list) > sum(self.current_splits_list):
+            self.best_splits_list = self.current_splits_list
             self.best_model = self.model
 
-        self.cross_val_score = sum(self.current_folds_list) / self.n_folds
-        self.current_folds_list = []
+        self.cross_val_score = sum(self.current_splits_list) / self.n_splits
+        self.current_splits_list = []
--- tests/test_all.py
+++ tests/test_all.py
@@ -9,9 +9,9 @@ def test_pruner_prun_yes():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(6):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
-    pruner.add_fold_value_and_prun(10000.0)
+    pruner.add_split_value_and_prun(10000.0)
 
     assert pruner.prun
 
@@ -21,10 +21,10 @@ def test_pruner_prun_no():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(4):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
     for i in range(3):
-        pruner.add_fold_value_and_prun(.6)
+        pruner.add_split_value_and_prun(.6)
 
     assert not pruner.prun
 
@@ -34,13 +34,13 @@ def test_pruner_prun_back():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(4):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
     for i in range(2):
-        pruner.add_fold_value_and_prun(10000.0)
+        pruner.add_split_value_and_prun(10000.0)
 
     for i in range(3):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
     assert not pruner.prun
 
@@ -50,9 +50,9 @@ def test_prun_first_run():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(4):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
-    assert pruner.best_folds_list == [1.0, 1.0, 1.0, 1.0]
+    assert pruner.best_splits_list == [1.0, 1.0, 1.0, 1.0]
 
 
 def test_prun_first_run_check():
@@ -60,7 +60,7 @@ def test_prun_first_run_check():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(4):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
     assert not pruner.first_run
 
@@ -81,7 +81,7 @@ def test_prun_vals_type():
 
     with pytest.raises(TypeError):
         pruner = PrunerCV(4, 0.1)
-        pruner.add_fold_value_and_prun(1)
+        pruner.add_split_value_and_prun(1)
 
 
 def test_prun_score_val_constant():
@@ -89,7 +89,7 @@ def test_prun_score_val_constant():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(8):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
     assert pruner.cross_val_score == 1.0
 
@@ -99,9 +99,9 @@ def test_prun_score_val_dec():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(7):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
-    pruner.add_fold_value_and_prun(.9)
+    pruner.add_split_value_and_prun(.9)
 
     assert pruner.cross_val_score < 1.0
 
@@ -111,9 +111,9 @@ def test_prun_score_val_inc():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(7):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
-    pruner.add_fold_value_and_prun(1.1)
+    pruner.add_split_value_and_prun(1.1)
 
     assert pruner.cross_val_score > 1.0
 
@@ -123,11 +123,11 @@ def test_prun_score_val_best():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(7):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
-    pruner.add_fold_value_and_prun(1.1)
+    pruner.add_split_value_and_prun(1.1)
 
-    assert sum(pruner.best_folds_list) / pruner.n_folds == 1.0
+    assert sum(pruner.best_splits_list) / pruner.n_splits == 1.0
 
 
 def test_prun_pruned_cv_score():
@@ -135,10 +135,10 @@ def test_prun_pruned_cv_score():
     pruner = PrunerCV(4, 0.1)
 
     for i in range(4):
-        pruner.add_fold_value_and_prun(1.0)
+        pruner.add_split_value_and_prun(1.0)
 
     for i in range(2):
-        pruner.add_fold_value_and_prun(2.0)
+        pruner.add_split_value_and_prun(2.0)
 
     assert pruner.cross_val_score == 2.0
 
@@ -149,7 +149,7 @@ def test_prun_3models():
     x = data['data']
     y = data['target']
 
-    pruner = PrunerCV(n_folds=8, tolerance=.1)
+    pruner = PrunerCV(n_splits=8, tolerance=.1)
 
     model1 = LGBMRegressor(max_depth=25)
     model2 = LGBMRegressor(max_depth=10)
