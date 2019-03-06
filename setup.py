da19f6a29e9bd7fb8ac91dd8eb83b117fde92b20
Piotr Gabrys <piotrgabrys@Piotrs-MacBook-Pro.local>
2019-03-06 10:27:11 +0100

    requirements added

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author_email='piotrek.gabrys@gmail.com',
     version='0.1',
     packages=['prunedcv',],
+    install_requires=['scikit-learn>=0.20.2'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     url="https://github.com/PiotrekGa/pruned-cv"
