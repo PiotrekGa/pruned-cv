df0e2e1fb0a5673b8f49096983eb66bf96428588
piotrgabrys <piotr.gabrys@samba.tv>
2019-03-11 07:35:42 -0700

    version change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='prunedcv',
     author='Piotr Gabryś',
     author_email='piotrek.gabrys@gmail.com',
-    version='0.1',
+    version='0.0.1',
     packages=['prunedcv',],
     install_requires=[
         'scikit-learn>=0.20.2',
